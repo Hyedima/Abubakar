022e388c5466477b975ff6497ed528b5d9dffd26
Hyedima <hyedimawaha@gmail.com>
2019-09-12 21:56:48 +0100

    Initial commit

---
+++ TwilioApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+using Twilio.TwiML;
+
+namespace TwilioPhone
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void MakeCall()
+        {
+            // Your Account SID from twilio.com/console
+            var accountSid = "AC1f05db609ba8398a8b1c3664fb8f38d0";
+
+            // Your Auth Token from twilio.com/console
+            var authToken = "d82287ecde01621d6a97454ed3ae12c0";
+
+            try
+            {
+                TwilioClient.Init(accountSid, authToken);
+
+                var call = CallResource.Create(
+                    to: new PhoneNumber("+2348036420271"),
+                    from: new PhoneNumber("+2347037736311"),
+                    url: new Uri("http://27276fbf.ngrok.io/default/"));
+
+                //Console.WriteLine(call.Sid.ToString());
+                //Console.Write("Press any key to continue.");
+                //Console.ReadKey();
+            }
+            catch (Exception n)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ TwilioApp/TwilioClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.TwiML;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+
+namespace TwilioApp
+{
+    class TwilioClass
+    {
+        public TwilioClass()
+        {
+        }
+
+        public void MakeCall(string personName)
+        {
+            // Your Account SID from twilio.com/console
+            var accountSid = "AC1f05db609ba8398a8b1c3664fb8f38d0";
+
+            // Your Auth Token from twilio.com/console
+            var authToken = "d82287ecde01621d6a97454ed3ae12c0";
+
+            try
+            {
+                //TwilioClient.Init(accountSid, authToken);
+                //var call = CallResource.Create(
+                //    to: new PhoneNumber("+2348036420271"),
+                //    //to: new PhoneNumber("+2347037736311"),
+                //    from: new PhoneNumber("+2347037736311"),
+                //    url: new Uri("http://f491f4b1.ngrok.io/ids.ng/demo.xml"));
+
+                //string callID = call.Sid.ToString();
+
+                var twilio = new TwilioRestClient(accountSid, authToken);
+
+
+                CallOptions options = new CallOptions();
+
+                
+
+
+
+                //                var twilio = new TwilioRestClient(accountSid, authToken);
+
+                //              CallOptions options = new CallOptions();
+
+            }
+            catch (Exception n)
+            {
+
+            }
+
+
+        }
+    }
+}
